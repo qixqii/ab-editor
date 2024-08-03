4b963a27034caba5c21e9a0283d4719721f9f0d6
ᕦ(ò_óˇ)ᕤ <qixqii@hotmail.com>
2024-08-03 21:41:02 +0800

    feat: placeholder , todo .... type /

--- packages/ab-editor/src/AbEditor.tsx
+++ packages/ab-editor/src/AbEditor.tsx
@@ -2,8 +2,8 @@
 import { useEditor, EditorContent, FloatingMenu, BubbleMenu } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import "@/ab-editor.scss";
+import Placeholder from "@tiptap/extension-placeholder";
 
-// define your extension array
 // const extensions = [StarterKit];
 const extensions = [
   StarterKit.configure({
@@ -14,9 +14,12 @@ const extensions = [
       },
     },
   }),
+  Placeholder.configure({
+    placeholder: "Type / to browse options",
+  }),
 ];
 
-const content = "<p>Hello World!</p>";
+const content = "";
 
 const AbEditor = () => {
   const editor = useEditor({
@@ -27,30 +30,64 @@ const AbEditor = () => {
   return (
     <>
       <EditorContent editor={editor} />
-      <FloatingMenu editor={editor}>this is the floating menu</FloatingMenu>
-      <BubbleMenu editor={editor}>
-        <button onClick={() => editor.chain().focus().toggleBold().run()} className={editor.isActive("bold") ? "is-active" : ""}>
-          Bold
-        </button>
-        <button onClick={() => editor.chain().focus().toggleItalic().run()} className={editor.isActive("italic") ? "is-active" : ""}>
-          Italic
-        </button>
-        <button
-          onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-          className={editor.isActive("heading", { level: 1 }) ? "is-active" : ""}>
-          H1
-        </button>
-        <button
-          onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-          className={editor.isActive("heading", { level: 2 }) ? "is-active" : ""}>
-          H2
-        </button>
-        <button
-          onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
-          className={editor.isActive("heading", { level: 3 }) ? "is-active" : ""}>
-          H3
-        </button>
-      </BubbleMenu>
+      {editor && (
+        <FloatingMenu editor={editor} tippyOptions={{ duration: 100 }}>
+          <div className="floating-menu">
+            <button
+              onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+              className={editor.isActive("heading", { level: 1 }) ? "is-active" : ""}>
+              H1
+            </button>
+            <button
+              onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+              className={editor.isActive("heading", { level: 2 }) ? "is-active" : ""}>
+              H2
+            </button>
+            <button
+              onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+              className={editor.isActive("heading", { level: 3 }) ? "is-active" : ""}>
+              H3
+            </button>
+            <button onClick={() => editor.chain().focus().toggleBulletList().run()} className={editor.isActive("bulletList") ? "is-active" : ""}>
+              Bullet List
+            </button>
+            <button onClick={() => editor.chain().focus().toggleOrderedList().run()} className={editor.isActive("orderedList") ? "is-active" : ""}>
+              Numbered List
+            </button>
+            <button onClick={() => editor.chain().focus().toggleTaskList().run()} className={editor.isActive("taskList") ? "is-active" : ""}>
+              Task List
+            </button>
+            <button onClick={() => editor.chain().focus().toggleBlockquote().run()} className={editor.isActive("blockquote") ? "is-active" : ""}>
+              Blockquote
+            </button>
+          </div>
+        </FloatingMenu>
+      )}
+      {editor && (
+        <BubbleMenu editor={editor}>
+          <button onClick={() => editor.chain().focus().toggleBold().run()} className={editor.isActive("bold") ? "is-active" : ""}>
+            Bold
+          </button>
+          <button onClick={() => editor.chain().focus().toggleItalic().run()} className={editor.isActive("italic") ? "is-active" : ""}>
+            Italic
+          </button>
+          <button
+            onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+            className={editor.isActive("heading", { level: 1 }) ? "is-active" : ""}>
+            H1
+          </button>
+          <button
+            onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+            className={editor.isActive("heading", { level: 2 }) ? "is-active" : ""}>
+            H2
+          </button>
+          <button
+            onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+            className={editor.isActive("heading", { level: 3 }) ? "is-active" : ""}>
+            H3
+          </button>
+        </BubbleMenu>
+      )}
     </>
   );
 };

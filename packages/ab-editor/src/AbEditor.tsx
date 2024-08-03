3dcf87a5162da0c1a8fda38caceb378de3bc926d
ᕦ(ò_óˇ)ᕤ <qixqii@hotmail.com>
2024-08-03 21:10:16 +0800

    feat: bubble menu

--- packages/ab-editor/src/AbEditor.tsx
+++ packages/ab-editor/src/AbEditor.tsx
@@ -4,7 +4,17 @@ import StarterKit from "@tiptap/starter-kit";
 import "@/ab-editor.scss";
 
 // define your extension array
-const extensions = [StarterKit];
+// const extensions = [StarterKit];
+const extensions = [
+  StarterKit.configure({
+    heading: {
+      levels: [1, 2, 3], // 配置支持的标题级别
+      HTMLAttributes: {
+        class: "ab-heading", // 添加自定义类名
+      },
+    },
+  }),
+];
 
 const content = "<p>Hello World!</p>";
 
@@ -17,8 +27,30 @@ const AbEditor = () => {
   return (
     <>
       <EditorContent editor={editor} />
-      <FloatingMenu editor={editor}>This is the floating menu</FloatingMenu>
-      <BubbleMenu editor={editor}>This is the bubble menu</BubbleMenu>
+      <FloatingMenu editor={editor}>this is the floating menu</FloatingMenu>
+      <BubbleMenu editor={editor}>
+        <button onClick={() => editor.chain().focus().toggleBold().run()} className={editor.isActive("bold") ? "is-active" : ""}>
+          Bold
+        </button>
+        <button onClick={() => editor.chain().focus().toggleItalic().run()} className={editor.isActive("italic") ? "is-active" : ""}>
+          Italic
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+          className={editor.isActive("heading", { level: 1 }) ? "is-active" : ""}>
+          H1
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+          className={editor.isActive("heading", { level: 2 }) ? "is-active" : ""}>
+          H2
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+          className={editor.isActive("heading", { level: 3 }) ? "is-active" : ""}>
+          H3
+        </button>
+      </BubbleMenu>
     </>
   );
 };
